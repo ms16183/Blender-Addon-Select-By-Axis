bf54b4c8d292bba79ea135492e43a95877872333
tiro-finale <syubarutusiruto@icloud.com>
2020-06-05 21:51:19 +0900

    [update] any selection mode is available.

--- module.py
+++ module.py
@@ -1,58 +1,36 @@
 import bpy, bmesh
 import math, mathutils
 
-def select_axis(context, orientations, axis, offset):
+def select_axis(context, orientations="Global", axis="-X", offset=0.0):
     
-    bpy.ops.mesh.select_mode(type="VERT")
-
-    # Get all vertex.
+    # get all vertices.
     me = context.object.data      # return type: bpy.types.Mesh
     bm = bmesh.from_edit_mesh(me) # return type: bmesh.types.Bmesh
 
+    # display selection mark when select mode is changed.
+    bm.select_flush_mode()
+
     for v in bm.verts:
 
+        # update coordinates according to the orientations.
         x, y, z = v.co
-
         if orientations == "Global":
             mat = context.object.matrix_world
             x, y, z = mat @ v.co
+        elif orientations == "Local":
+            x, y, z = v.co
 
-        if axis == "-X":
-            if x < -offset:
-                v.select_set(True)
-            else:
-                v.select_set(False)
+        # select vertices.
+        if (axis == "-X" and x < -offset) or (axis == "+X" and x > offset) \
+        or (axis == "-Y" and y < -offset) or (axis == "+Y" and y > offset) \
+        or (axis == "-Z" and z < -offset) or (axis == "+Z" and z > offset):
+            v.select_set(True)
+        else:
+            v.select_set(False)
 
-        elif axis == "+X":
-            if x > offset:
-                v.select_set(True)
-            else:
-                v.select_set(False)
+    # select edges and faces made up of the selected vertices.
+    bpy.ops.mesh.select_more()
 
-        elif axis == "-Y":
-            if y < -offset:
-                v.select_set(True)
-            else:
-                v.select_set(False)
-
-        elif axis == "+Y":
-            if y > offset:
-                v.select_set(True)
-            else:
-                v.select_set(False)
-
-        elif axis == "-Z":
-            if z < -offset:
-                v.select_set(True)
-            else:
-                v.select_set(False)
-
-        elif axis == "+Z":
-            if z > offset:
-                v.select_set(True)
-            else:
-                v.select_set(False)
-
-
+    # update mesh from bmesh.
     bmesh.update_edit_mesh(me)
     me.update()
